55e546d23f0bc55718e5562e5839459e0c1f229c
Bill <billsnook@me.com>
2023-06-23 12:08:49 -0700

    Initial Commit

---
+++ RobotController/RobotControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotControllerApp.swift
+//  RobotController
+//
+//  Created by Bill Snook on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct RobotControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
