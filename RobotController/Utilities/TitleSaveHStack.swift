7cd1fd71adfaffda5a5b1ae6b274075fe945b8d0
Bill <billsnook@me.com>
2023-07-19 12:03:21 -0700

    Initial calibrate view

---
+++ RobotController/Utilities/TitleSaveHStack.swift
@@ -0,0 +1,54 @@
+//
+//  TitleSaveHStack.swift
+//  RobotController
+//
+//  Created by Bill Snook on 7/15/23.
+//
+
+import SwiftUI
+
+struct TitleSaveHStack: View {
+    let fileLead = "/home/data/"
+    let fileType = ".dat"
+    
+    let title: String
+    let fileName: String
+
+    var body: some View {
+        HStack {
+            Button("Reset") {
+                print("Reset button action")
+                resetFile()
+            }
+            .buttonStyle(.bordered)
+            
+            Spacer()
+            Text(title)
+            Spacer()
+            Button("Save") {
+                print("Save button action")
+                saveFile()
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+
+    private func getFilePath() -> String {
+        return fileLead + fileName + fileType
+    }
+
+    private func resetFile() {
+        print("Resetting file at \(getFilePath())")
+    }
+
+    private func saveFile() {
+        print("Saving file at \(getFilePath())")
+    }
+}
+
+struct TitleSaveHStack_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleSaveHStack(title: "Alignment", fileName: "AlignmentTest")
+            .padding(EdgeInsets(top: 4.0, leading: 20.0, bottom: 4.0, trailing: 20.0))
+    }
+}
--- RobotController/ViewControllers/CalibrateView.swift
+++ RobotController/ViewControllers/CalibrateView.swift
@@ -10,14 +10,24 @@ import SwiftUI
 struct CalibrateView: View {
     var body: some View {
         VStack {
-            Text("Calibrate!")
+            Text("Calibrate! 3")
+            VStack {
+                TitleSaveHStack(title: "Scanner Alignment", fileName: "ScannerAlignment")
+                    .padding(EdgeInsets(top: 4.0, leading: 0.0, bottom: 4.0, trailing: 0.0))
+            }
+            .background(Color.clear)
+            .cornerRadius(10.0)
+            VStack {
+                TitleSaveHStack(title: "Speed Alignment", fileName: "SpeedAlignment")
+            }
+            Spacer()
         }
-        .navigationBarTitle("Calibration", displayMode: .inline)
     }
 }
 
 struct CalibrateView_Previews: PreviewProvider {
     static var previews: some View {
         CalibrateView()
+            .padding(EdgeInsets(top: 4.0, leading: 20.0, bottom: 4.0, trailing: 20.0))
     }
 }
